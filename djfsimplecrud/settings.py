8a7194291153e77948f64bbe61cdefa7a9c94d11
Skatetablet <skatewer@gmail.com>
2024-08-14 17:42:17 -0700

    updating

--- djfsimplecrud/settings.py
+++ djfsimplecrud/settings.py
@@ -127,10 +127,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-DEBUG = False
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+DEBUG = os.getenv('DJANGO_DEBUG', 'False') == 'True'
+
 
 if not DEBUG:    
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
@@ -139,3 +141,4 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
